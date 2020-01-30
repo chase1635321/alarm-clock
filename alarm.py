28546c99ae671fd9fe162c9b01617fde2d13414f
Chase Kanipe crkanip <crkanip@ip-10-113-155-69.evoforge.org>
2020-01-30 19:22:41 +0000

    Automatic push

--- alarm.py
+++ alarm.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+import npyscreen
+
+class TestApp(npyscreen.NPSApp):
+
+    def main(self):
+        # These lines create the form and populate it with widgets.
+        form  = npyscreen.Form(name = "Jarvis v1.0",)
+        with open("asm.txt", "r") as f:
+            asmText = f.read()
+        form.add(npyscreen.TitleText, name="First Widget")
+        form.add(npyscreen.MultiLineEdit, name="Assembly Challenge", value=asmText, max_height=20)
+
+        # This lets the user interact with the Form.
+        form.edit()
+        print(ms.get_selected_objects())
+
+App = TestApp()
+App.run()
+
